5c01910e770f91c8b57441be98a66ab65d56af6a
muntasir14 <30627757+muntasir14@users.noreply.github.com>
2019-01-03 01:13:38 +0600

    All features are included.
     Minimum requirements are fulfilled.

--- main.c
+++ main.c
@@ -64,8 +64,9 @@ void addFriend(node *user1, node *user2){
         }else if((strcmp(user1->name, temp->name) == 0) && (strcmp(user1->location, temp->location) == 0) && (user1->birthyear == temp->birthyear)){
             actualUser1 = temp;
         }
-        if((actualUser1 != NULL) && (actualUser2 != NULL))
+        if((actualUser1 != NULL) && (actualUser2 != NULL)){
             break;
+        }
     }
     
     for(node *temp = actualUser1->nextFriend; temp != NULL; temp = temp->nextFriend){
@@ -77,10 +78,6 @@ void addFriend(node *user1, node *user2){
             return;
         }
     }
-    
-    printf("error before\n");
-    
-    // check if user1 and user2 exists
     
     
     
@@ -134,15 +131,126 @@ node* getUsers(){
     return users;
 }
 
-void showOptions(){
+void removeUser(node *user){
     
-    printf("Enter 1 to see all users\n");
-    printf("Enter 2 to see add a user\n");
-    printf("Enter 3 to see add friends\n");
-    printf("Enter 4 to see total users\n");
-    printf("Enter 5 to see user who has maximum friends\n");
+    // removing from everyone's friendlist
+    for(node *temp1 = users->next; temp1 != NULL; temp1 = temp1->next){
+        
+        node *temp2 = temp1;
+        for(node *temp3 = temp1->nextFriend; temp3 != NULL; temp3 = temp3->nextFriend, temp2 = temp2->nextFriend){
+            
+            if((strcmp(user->name, temp3->name) == 0) && (strcmp(user->location, temp3->location) == 0) && (user->birthyear == temp3->birthyear)){
+                
+                temp2->nextFriend = temp3->nextFriend;
+                free(temp3);
+                break;
+            }
+        }
+    }
+    
+    for(node *temp1 = users, *temp2 = users->next; temp2 != NULL; temp2 = temp2->next, temp1 = temp1->next){
+        
+        if((strcmp(user->name, temp2->name) == 0) && (strcmp(user->location, temp2->location) == 0) && (user->birthyear == temp2->birthyear)){
+            
+            temp1->next = temp2->next;
+            free(temp2);
+            break;
+        }
+    }
+    
+    
 }
 
+void commonFriends(node *user1, node *user2){
+    
+    node *actualUser1 = NULL, *actualUser2 = NULL;
+    node *commonFriends = (node*) malloc(sizeof(node));
+    commonFriends->next = NULL;
+    
+    for(node *temp = users->next; temp != NULL; temp = temp->next){
+        
+        if((strcmp(user2->name, temp->name) == 0) && (strcmp(user2->location, temp->location) == 0) && (user2->birthyear == temp->birthyear)){
+            actualUser2 = temp;
+        }else if((strcmp(user1->name, temp->name) == 0) && (strcmp(user1->location, temp->location) == 0) && (user1->birthyear == temp->birthyear)){
+            actualUser1 = temp;
+        }
+        if((actualUser1 != NULL) && (actualUser2 != NULL))
+            break;
+    }
+    for(node *temp1 = actualUser1->nextFriend; temp1 != NULL; temp1 = temp1->nextFriend){
+        
+        for(node *temp2 = actualUser2->nextFriend; temp2 != NULL; temp2 = temp2->nextFriend){
+            
+            if((strcmp(temp1->name, temp2->name) == 0) && (strcmp(temp1->location, temp2->location) == 0) && (temp1->birthyear == temp2->birthyear)){
+                
+                node *temp3 = (node*) malloc(sizeof(node));
+                strcpy(temp3->name, temp1->name);
+                strcpy(temp3->location, temp1->location);
+                temp3->birthyear = temp1->birthyear;
+                temp3->next = commonFriends->next;
+                commonFriends->next = temp3;
+            }
+        }
+    }
+    if(commonFriends->next == NULL){
+        printf("No Common Friends\n");
+        return;
+    }
+    printf("Common Friend(s) between %s and %s: ", user1->name, user2->name);
+    for(node *temp = commonFriends->next; temp != NULL; temp = temp->next)
+        printf("%s\t", temp->name);
+    printf("\n");
+}
+
+void printOldestFriends(){
+    
+    printf("\n\nName\t\tOldest friend\n\n");
+    for(node *temp1 = users->next; temp1 != NULL; temp1 = temp1->next){
+        
+        node *dummy = temp1->nextFriend;
+        if(dummy != NULL){
+            for(node *temp2 = temp1->nextFriend; temp2 != NULL; temp2 = temp2->nextFriend){
+                
+                if(dummy->birthyear > temp2->birthyear)
+                    dummy = temp2;
+            }
+            printf("%s\t\t%s\n", temp1->name, dummy->name);
+        }else{
+            
+            printf("%s\t\tNo friends\n", temp1->name);
+        }
+        
+    }
+    
+}
+
+void showOptions(){
+    
+    printf("\n\nEnter 1 to see all users\n");
+    printf("Enter 2 to add an user\n");
+    printf("Enter 3 to add friends\n");
+    printf("Enter 4 to see total users\n");
+    printf("Enter 5 to see user who has maximum friends\n");
+    printf("Enter 6 to remove an user\n");
+    printf("Enter 7 to see oldest friends\n");
+    printf("Enter 8 to see common friends\n\n");
+}
+
+
+node* setUserAndGet(){
+    
+    node *user = (node*) malloc(sizeof(node));
+    printf("Name: ");
+    scanf("%s", user->name);
+    printf("Location: ");
+    scanf("%s", user->location);
+    printf("Birthyear: ");
+    scanf("%d", &user->birthyear);
+    
+    user->nextFriend = NULL;
+    
+    return user;
+}
 
 
 void controlPanel(){
@@ -161,47 +269,40 @@ void controlPanel(){
                 break;
             case 2:
                 
-                temp = (node*) malloc(sizeof(node));
-                printf("Name: ");
-                scanf("%s", temp->name);
-                printf("Location: ");
-                scanf("%s", temp->location);
-                printf("Birthyear: ");
-                scanf("%d", &temp->birthyear);
-                temp->nextFriend = NULL;
+                temp = setUserAndGet();
                 addUser(temp);
                 //free(temp);
                 break;
             case 3:
                 printf("For first friend\n");
-                temp1 = (node*) malloc(sizeof(node));
-                printf("Name: ");
-                scanf("%s", temp1->name);
-                printf("Location: ");
-                scanf("%s", temp1->location);
-                printf("Birthyear: ");
-                scanf("%d", &temp1->birthyear);
-                temp1->nextFriend = NULL;
+                temp1 = setUserAndGet();
                 
                 printf("For second friend\n");
-                temp2 = (node*) malloc(sizeof(node));
-                printf("Name: ");
-                scanf("%s", temp2->name);
-                printf("Location: ");
-                scanf("%s", temp2->location);
-                printf("Birthyear: ");
-                scanf("%d", &temp2->birthyear);
-                temp2->nextFriend = NULL;
+                temp2 = setUserAndGet();
                 
                 addFriend(temp1, temp2);
-                //free(temp1);
-                //free(temp2);
+                
                 break;
             case 4:
                 printf("\n\nTotal Users: %d\n\n", totalUsers());
                 break;
             case 5:
                 printf("\n\nName: %s\n\n", mostFriends()->name);
+                break;
+            case 6:
+                temp = setUserAndGet();
+                removeUser(temp);
+                break;
+            case 7:
+                printOldestFriends();
+                break;
+            case 8:
+                printf("For first friend\n");
+                temp1 = setUserAndGet();
+                
+                printf("For second friend\n");
+                temp2 = setUserAndGet();
+                commonFriends(temp1, temp2);
                 break;
             default:
                 printf("Invalid Option!\n");
